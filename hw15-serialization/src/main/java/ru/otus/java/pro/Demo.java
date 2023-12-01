1f7d8fe89c8c0ccd93bd12268d7fdcb286657dee
rodioncikita <chikita.rodion@gmail.com>
2023-12-01 17:47:06 +0300

    1

--- hw15-serialization/src/main/java/ru/otus/java/pro/Demo.java
+++ hw15-serialization/src/main/java/ru/otus/java/pro/Demo.java
@@ -8,20 +8,22 @@ import ru.otus.java.pro.oldstructure.SmsData;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Demo {
     public static void main(String[] args) {
+        Logger log = Logger.getLogger(Demo.class.getName());
         Deserializer deserializer = new FileDeserializer();
         Serializer serializer = new FileSerializer();
         Processor processor = new ProcessorAggregator();
-        try (FileReader fileReader = new FileReader("/Users/rodioncikita/Downloads/sms.json")) {
+        try (FileReader fileReader = new FileReader("sms.json")) {
             SmsData smsData = deserializer.data(fileReader);
             SmsDataNewStructure smsDataNewStructure = processor.process(smsData);
-            System.out.println(smsDataNewStructure);
+            log.info(smsDataNewStructure.toString());
             serializer.serialize(smsDataNewStructure);
 
         } catch (IOException | ParseException exception) {
-            exception.printStackTrace();
+            log.info(exception.getMessage());
         }
     }
 }
